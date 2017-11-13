b62c5c61db4ce341e7189005effc9ff4d53ceb80
qyka1210 <qyka1210@gmail.com>
2017-11-13 14:41:35 -0800

    Add files via upload

---
+++ polyGo/polyGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  polyGo
+//
+//  Created by Arturo Lira on 11/13/17.
+//  Copyright © 2017 Bits Please. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
